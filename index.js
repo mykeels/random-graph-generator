15910938f37381d3ccda3200c23be189f11d11ea
Ikechi Michael <mykehell123@gmail.com>
2020-05-19 15:04:47 +0100

    chore: init npm package

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * generates a graph around supplied nodes
+ * 
+ * @param {Array} nodes An array of data to be hidden in the generated graph
+ * @param {{ maxDepth: number, maxWidth: number, nodeCount: number }} options 
+ */
+function generateRandomGraph(nodes, options) {
+
+}
+
+module.exports = generateRandomGraph;
