a3cbf7149c414999235d6c12c3da500f41845778
Ikechi Michael <mykehell123@gmail.com>
2020-05-19 15:12:27 +0100

    feat: add validations

--- index.js
+++ index.js
@@ -4,8 +4,12 @@
  * @param {Array} nodes An array of data to be hidden in the generated graph
  * @param {{ maxDepth: number, maxWidth: number, nodeCount: number }} options 
  */
-function generateRandomGraph(nodes, options) {
+function generateRandomGraph(nodes, { maxDepth, maxWidth, nodeCount }) {
+    if (!Math.floor(nodeCount)) throw new Error('[nodeCount] must be a positive integer');
+    if (!Math.floor(maxDepth)) throw new Error('[maxDepth] must be a positive integer');
+    if (!Math.floor(maxWidth)) throw new Error('[maxWidth] must be a positive integer');
 
+    if (!nodes || !Array.isArray(nodes) || !nodes.length) return [];
 }
 
 module.exports = generateRandomGraph;
