6702ea8df5b5e4ff72af656374a24153d4120d0d
mykeels <mykehell123@gmail.com>
2020-07-30 13:57:44 +0100

    feat: generate in chunks

--- example-graph.js
+++ example-graph.js
@@ -1,14 +1,45 @@
-const generate = require("./index");
+const { generateRandomGraph } = require("./index");
+
+const NODES = 5000;
+const maxWidth = 5;
+const edgeProbability = 0.3;
+
+const CHUNKS = 500;
+const graphs = [];
+
+let remainingNodes = NODES;
+
+while (remainingNodes > 0) {
+  const nodeCount = remainingNodes >= CHUNKS ? CHUNKS : (remainingNodes % CHUNKS);
+  const graph = generateRandomGraph(
+    new Array(nodeCount).fill(null).map((_, i) => (i + ((graphs.length) * CHUNKS)).toString()),
+    {
+      maxWidth,
+      edgeProbability,
+    }
+  );
+  graphs.push(graph);
+  remainingNodes -= CHUNKS;
+}
+
+const graph = { nodes: [], links: [] }
+
+graphs.forEach(g => {
+  const s = g.serialize();
+  if (graph.nodes.length) {
+    graph.links.push({
+      source: graph.nodes[0].id,
+      target: s.nodes[0].id,
+      weight: 1
+    });
+  }
+  graph.nodes = graph.nodes.concat(s.nodes);
+  graph.links = graph.links.concat(s.links);
+});
 
 console.log(
   JSON.stringify(
-    generate(
-      new Array(30).fill(null).map((_, i) => i.toString()),
-      {
-        maxWidth: 4,
-        edgeProbability: 0.3,
-      }
-    ).serialize(),
+    graph,
     null,
     2
   )
--- example-tree.js
+++ example-tree.js
@@ -1,15 +1,63 @@
-const { generateRandomTree, generateRandomGraph } = require("./index");
+const { generateRandomTree } = require("./index");
+
+const NODES = 50;
+const maxWidth = 3;
+const edgeProbability = 0.5;
+
+const CHUNKS = 10;
+const trees = [];
+
+let remainingNodes = NODES;
+
+while (remainingNodes > 0) {
+  const nodeCount = remainingNodes >= CHUNKS ? CHUNKS : (remainingNodes % CHUNKS);
+  const graph = generateRandomTree(
+    new Array(nodeCount).fill(null).map((_, i) => (i + ((trees.length) * CHUNKS)).toString()),
+    {
+      maxWidth,
+      edgeProbability,
+    }
+  );
+  trees.push(graph);
+  remainingNodes -= CHUNKS;
+}
+
+const tree = { nodes: [], links: [] }
+
+trees.forEach(g => {
+  const s = g.serialize();
+  if (tree.nodes.length) {
+    tree.links.push({
+      source: tree.nodes[0].id,
+      target: s.nodes[0].id,
+      weight: 1
+    });
+  }
+  tree.nodes = tree.nodes.concat(s.nodes);
+  tree.links = tree.links.concat(s.links);
+});
 
 console.log(
   JSON.stringify(
-    generateRandomTree(
-      new Array(1000).fill(null).map((_, i) => i.toString()),
-      {
-        maxWidth: 3,
-        edgeProbability: 0.5,
-      }
-    ).serialize(),
+    tree,
     null,
     2
   )
 );
+
+
+// const { generateRandomTree, generateRandomGraph } = require("./index");
+
+// console.log(
+//   JSON.stringify(
+//     generateRandomTree(
+//       new Array(3000).fill(null).map((_, i) => i.toString()),
+//       {
+//         maxWidth: 3,
+//         edgeProbability: 0.5,
+//       }
+//     ).serialize(),
+//     null,
+//     2
+//   )
+// );
