966ccdb56485478944da57b28d1b7ce433ca1ea3
mykeels <mykehell123@gmail.com>
2020-07-30 13:58:12 +0100

    chore: rm comments

--- example-tree.js
+++ example-tree.js
@@ -44,20 +44,3 @@ console.log(
     2
   )
 );
-
-
-// const { generateRandomTree, generateRandomGraph } = require("./index");
-
-// console.log(
-//   JSON.stringify(
-//     generateRandomTree(
-//       new Array(3000).fill(null).map((_, i) => i.toString()),
-//       {
-//         maxWidth: 3,
-//         edgeProbability: 0.5,
-//       }
-//     ).serialize(),
-//     null,
-//     2
-//   )
-// );
